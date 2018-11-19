203cd21e809ebb41314bcb6545cf6e92c557dddd
Jonathan Negron <jnegron@broadway.com>
2018-11-19 13:32:06 -0500

    Update

--- inlines/parser.py
+++ inlines/parser.py
@@ -11,62 +11,39 @@ def inlines(value, return_list=False):
     print("VALUE:")
     print(value)
     try:
-        print("7")
         from bs4 import BeautifulSoup
     except ImportError:
-        print("8")
         pass
 
-    content = BeautifulSoup(value, "lxml")
-    print(content)
+    content = BeautifulSoup(value)
 
     # Return a list of inline objects found in the value.
     if return_list:
-        print("9")
         inline_list = []
         for inline in content.findAll('inline'):
             rendered_inline = render_inline(inline)
             inline_list.append(rendered_inline['context'])
-        print("10")
-        print(inline_list)
         return inline_list
 
     # Replace inline markup in the value with rendered inline templates.
     else:
-        print("11")
         for inline in content.findAll('inline'):
             new_inline = "<inline "
             for attr in inline.attrs:
                 new_inline+= str(attr) + "=\"" + str(inline[attr]) + "\" "
-                print("NEW INLINE")
-                print(new_inline)
             new_inline += "/>"
-            print("FINAL NEWLINE")
-            print(new_inline)
             # self_closing_inline = str(new_inline)[:-10] + "/>"
-            # print(self_closing_inline)
             rendered_inline = render_inline(inline)
             if rendered_inline:
-                print("RENDERED_INLINE")
-                print(rendered_inline)
                 inline_template = render_to_string(rendered_inline['template'],
                                                    rendered_inline['context'])
-                print("INLINE_TEMPLATE")
-                print(inline_template)
             else:
                 inline_template = ''
-            print("INLINE")
-            print(new_inline)
-            print("THIS A TEST YAHURRDDDD")
-            print(value.replace(new_inline, inline_template))
             value = value.replace(new_inline, inline_template)
-        print("12")
-        print(mark_safe(str(value)))
         return mark_safe(str(value))
 
 
 def render_inline(inline):
-    print("In render_inline")
     """
     Replace inline markup with template markup that matches the
     appropriate app and model.
@@ -74,7 +51,6 @@ def render_inline(inline):
 
     # Look for inline type, 'app.model'
     try:
-        print("1")
         app_label, model_name = inline['type'].split('.')
     except:
         if settings.DEBUG:
@@ -85,40 +61,29 @@ def render_inline(inline):
 
     # Look for content type
     try:
-        print("2")
         content_type = ContentType.objects.get(app_label=app_label,
                                                model=model_name)
         model = content_type.model_class()
-        print(content_type)
-        print(model)
     except ContentType.DoesNotExist:
-        print("CONTENT DOES NOT EXIST")
         if settings.DEBUG:
             raise TemplateSyntaxError("Inline ContentType not found.")
         else:
             return ''
 
     # Create the context with all the attributes in the inline markup.
-    print("3")
     context = dict()
     # context = dict((attr[0], attr[1]) for attr in inline.attrs)
-    print(list(inline.attrs))
     for attr in list(inline.attrs):
-        print(attr)
-        print(inline[attr])
         context[attr] = inline[attr]
-    print(context)
 
     # If multiple IDs were specified, build a list of all requested objects
     # and add them to the context.
     try:
         try:
-            print("4")
             id_list = [int(i) for i in inline['ids'].split(',')]
             obj_list = model.objects.in_bulk(id_list)
             obj_list = list(obj_list[int(i)] for i in id_list)
             context['object_list'] = obj_list
-            print(context)
         except ValueError:
             if settings.DEBUG:
                 raise ValueError("The <inline> ids attribute is missing or "
@@ -130,22 +95,16 @@ def render_inline(inline):
     # to the context.
     except KeyError:
         try:
-            print("5")
             obj = model.objects.get(pk=inline['id'])
-            print(inline['id'])
-            print(obj)
             context['object'] = obj
             context['settings'] = settings
-            print(context)
         except model.DoesNotExist:
-            print("MODEL AINT REAL")
             if settings.DEBUG:
                 raise model.DoesNotExist("%s with pk of '%s' does not exist"
                                          % (model_name, inline['id']))
             else:
                 return ''
         except:
-            print("ITS SLOW B")
             if settings.DEBUG:
                 raise TemplateSyntaxError("The <inline> id attribute is "
                                           "missing or invalid.")
@@ -153,7 +112,6 @@ def render_inline(inline):
                 return ''
 
     # Set the name of the template that should be used to render the inline.
-    print("6")
     template = ["inlines/%s_%s.html" % (app_label, model_name),
                 "inlines/default.html"]
 
